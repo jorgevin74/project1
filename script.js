0c4324035fc5b0137c82f0cc6d73e80fd1fdd176
Jorge Auquilla <jorge.auquilla99@myhunter.cuny.edu>
2025-09-23 22:48:15 -0400

    update

--- script.js
+++ script.js
@@ -1,53 +1,38 @@
+gsap.registerPlugin(SplitText) 
 
-const heart = document.getElementById('health');
-const heart_2 = document.getElementById('health-2');
-const heart_3 = document.getElementById('health-3');
-const heart_4 = document.getElementById('health-4');
-const heart_5 = document.getElementById('health-5');
+const title_card = document.getElementById("title");
+const strtBtn = document.getElementById("start");
+const text = document.getElementById("prompt");
 
-const title_card = document.getElementById('title');
-const msg = document.getElementById('message');
+const option_1 = document.getElementById("option_1");
+const option_2 = document.getElementById("option_2");
+const option_3 = document.getElementById("option_3");
 
-const main_char = document.getElementById('main_character');
+// split elements with the class "split" into words and characters
 
-const StartBtn = document.getElementById('start');
-const restartBtn = document.getElementById('restart');
+function split(){
+  split = SplitText.create(".text-container", { type: "words, chars, lines" });
+  return split;
+}
 
-var currentSize = parseInt(window.getComputedStyle(main_char).fontSize, 10);
-
-StartBtn.addEventListener('click', function() {
-    console.log(currentSize);
-    for(let x = 0; x < 5; x++){
-        if(main_char.style.fontSize == (1) + "em"){
-            heart.style.visibility = 'visible';
-            console.log("is it working?");
-        }
-        if(main_char.style.fontSize == (2) + "em"){
-            heart_2.style.visibility = 'visible';
-        }
-        if(main_char.style.fontSize == (3) + "em"){
-            heart_3.style.visibility = 'visible';
-        }
-        if(main_char.style.fontSize == (4) + "em"){
-            heart_4.style.visibility = 'visible';
-        }
-        if(main_char.style.fontSize == (5) + "em"){
-            heart_5.style.visibility = 'visible';
-        }
-        main_char.style.fontSize = (currentSize + x) + "em";
-        console.log(main_char.style.fontSize);
-    }
-    title_card.style.visibility = 'hidden';
-    msg.style.visibility = 'hidden';
+strtBtn.addEventListener("click", () => {
+  // reveal UI first
+  option_1.style.visibility = "visible";
+  option_2.style.visibility = "visible";
+  option_3.style.visibility = "visible";
+  title_card.style.visibility = "hidden";
+  strtBtn.style.visibility = "hidden";
+  text.style.visibility = "visible";
+  split();
+  // animate words
+  gsap.from(split.lines, {
+    duration: 2,
+    y: 100,
+    autoAlpha: 0,
+    stagger: 0.05
+  });
 });
 
-restartBtn.addEventListener('click', function() {
-    heart.style.visibility = 'hidden';
-    heart_2.style.visibility = 'hidden';
-    heart_3.style.visibility = 'hidden';
-    heart_4.style.visibility = 'hidden';
-    heart_5.style.visibility = 'hidden';
-    title_card.style.visibility = 'visible';
-    msg.style.visibility = 'visible';
-});
+
+
 
